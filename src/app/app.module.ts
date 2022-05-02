4c91c27304074009cf6b4f9cc082f6ac515f5c2b
vijaykumar <vijay1111mane@gmail.com>
2022-05-02 17:08:12 +0530

    commit;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddStudentsComponent } from './add-students/add-students.component';
+import { ListStudentsComponent } from './list-students/list-students.component';
+import { EditStudentsComponent } from './edit-students/edit-students.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddStudentsComponent,
+    ListStudentsComponent,
+    EditStudentsComponent
   ],
   imports: [
     BrowserModule,
